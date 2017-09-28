f5f7a1447d9ba0baae447c3ffecaded5ab2b824e
Tim Strijdhorst <tim@decorrespondent.nl>
2017-09-28 23:30:48 +0200

    +get the list

--- KeybaseAPIClient.php
+++ KeybaseAPIClient.php
@@ -1,12 +1,30 @@
 <?php
 
 
+use React\EventLoop\LoopInterface;
+
 class KeybaseAPIClient {
+	/**
+	 * @var LoopInterface
+	 */
+	private $loop;
+	
+	public function __construct(LoopInterface $loop) {
+		$this->loop = $loop;
+	}
+	
 	/**
 	 * @return array
 	 */
 	public function getNewMessages() : array {
-	
+		$listCommand = [
+			'method' => 'list'
+		];
+		
+		$listCommandJson = json_encode($listCommand);
+		exec('keybase chat api -m '.escapeshellarg($listCommandJson), $output);
+		
+		var_dump($output);
 	}
 	
 	public function sendMessage(string $channel, string $message) {
--- bot.php
+++ bot.php
@@ -2,17 +2,19 @@
 
 $loop = React\EventLoop\Factory::create();
 
-$keybaseApi = new KeybaseAPIClient();
-$giphyApi = new GiphyAPIClient();
+$keybaseApi = new KeybaseAPIClient($loop);
+$giphyApi   = new GiphyAPIClient();
 
-$loop->addPeriodicTimer(1, function () use ($keybaseApi, $giphyApi) {
-	foreach($keybaseApi->getNewMessages() as $message) {
+$loop->addPeriodicTimer(
+	3, function () use ($keybaseApi, $giphyApi) {
+	foreach ($keybaseApi->getNewMessages() as $message) {
 		if (substr($message->body, 0, 7) === '/giphy ') {
 			$searchQuery = substr($message->body, 7); //@todo this might be off by 1 at the start
-			$randomGif = $giphyApi->getRandomGif($searchQuery);
+			$randomGif   = $giphyApi->getRandomGif($searchQuery);
 			$keybaseApi->uploadFile($message->channel, $randomGif);
 		}
 	}
-});
+}
+);
 
 $loop->run();
