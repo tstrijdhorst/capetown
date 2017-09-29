9fb3987bd1aab0e1e5c03f6c4fb86487e302997b
Tim Strijdhorst <tim@decorrespondent.nl>
2017-09-29 20:56:34 +0200

    %move into different folders to start namespacing

--- app/bot.php
+++ app/bot.php
@@ -12,11 +12,11 @@ $giphyApi   = new GiphyAPIClient();
 $loop->addPeriodicTimer(
 	3, function () use ($keybaseApi, $giphyApi) {
 	foreach ($keybaseApi->getUnreadMessages() as $message) {
-//		if (substr($message['body'], 0, 7) === '/giphy ') {
-//			$searchQuery = substr($message['body'], 7); //@todo this might be off by 1 at the start
-//			$randomGif   = $giphyApi->getRandomGif($searchQuery);
-//			$keybaseApi->uploadFile($message['channel'], $randomGif);
-//		}
+		if (substr($message['body'], 0, 7) === '/giphy ') {
+			$searchQuery = substr($message['body'], 7); //@todo this might be off by 1 at the start
+			$randomGif   = $giphyApi->getRandomGif($searchQuery);
+			$keybaseApi->uploadFile($message['channel'], $randomGif);
+		}
 		var_dump($message);
 	}
 }
