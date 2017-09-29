b7465a11ba8ed72b9b61d859c679af9b4b49459b
Tim Strijdhorst <tim@decorrespondent.nl>
2017-09-29 21:59:30 +0200

    +create tempdir if necessary

--- app/Plugins/Giphy/GiphyAPIClient.php
+++ app/Plugins/Giphy/GiphyAPIClient.php
@@ -3,6 +3,8 @@
 namespace Capetown\Plugins\Giphy;
 
 class GiphyAPIClient {
+	private const TEMPDIR = \Capetown\TEMP_DIR.'/gifs/';
+	
 	/**
 	 * @param $searchQuery
 	 * @return \SplFileObject
@@ -12,7 +14,7 @@ class GiphyAPIClient {
 		$searchResultsRaw = json_decode(file_get_contents('https://api.giphy.com/v1/gifs/search?api_key=d2evT7jdFzVkllsBjRZ6yyDLO0nZulNh&q='.urlencode($searchQuery).'&limit=25&offset=0&rating=G&lang=en'), true)['data'];
 		
 		if (count($searchResultsRaw) === 0) {
-			throw new NoSearchResultsFoundException();
+			throw new NoSearchResultsFoundException($searchQuery);
 		}
 		
 		$randomSearchResultRaw = $searchResultsRaw[rand(0, count($searchResultsRaw) - 1)];
@@ -22,9 +24,13 @@ class GiphyAPIClient {
 			throw new \Exception('Could not download gif');
 		}
 		
-		$fileName = \Capetown\TEMP_DIR.'/gifs/'.$randomSearchResultRaw['id'].'gif';
+		if (file_exists(self::TEMPDIR) === false) {
+			mkdir(self::TEMPDIR);
+		}
+		
+		$fileName = self::TEMPDIR.$randomSearchResultRaw['id'].'.gif';
 		file_put_contents($fileName, $randomGif);
 		
-		$fileObject = new \SplFileObject($fileName);
+		return new \SplFileObject($fileName);
 	}
 }
