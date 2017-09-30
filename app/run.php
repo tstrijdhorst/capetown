21a4e8c49650fd131dd03604af945d08adede306
Tim Strijdhorst <tim@decorrespondent.nl>
2017-09-30 22:21:17 +0200

    +refactor runner to class +introduce runner config so that the core is completely decoupled from the runner

--- app/run.php
+++ app/run.php
@@ -3,22 +3,43 @@
 namespace Capetown\Runner;
 
 use Capetown\Core\Bot;
+use Capetown\Core\CoreConfig;
 use Capetown\Core\KeybaseAPIClient;
 use Dotenv\Dotenv;
 
 require_once __DIR__.'/../vendor/autoload.php';
-define('Capetown\VERBOSE', true);
 
-$configDir = __DIR__.'/../config';
-foreach(EnabledCommands::getEnabledCommandClasses() as $commandClass) {
-	$fileName = $commandClass::getName().'.env';
-	if (is_file($configDir.'/'.$fileName)) {
-		$dotEnv = new Dotenv($configDir, $commandClass::getName().'.env');
-		$dotEnv->load();
+Runner::run();
+
+class Runner {
+	private const CONFIG_DIR = __DIR__.'/../config';
+	
+	public static function run() {
+		self::importConfiguration();
+		self::exportCoreConfig();
+		
+		$bot = new Bot(new KeybaseAPIClient(), EnabledCommands::getEnabledCommandClasses());
+		$bot->run();
+	}
+	
+	private static function importConfiguration(): void {
+		$configFileNames = ['.env'];
+		foreach (EnabledCommands::getEnabledCommandClasses() as $commandClass) {
+			$configFileName        = $commandClass::getName().'.env';
+			$commandConfigFilePath = self::CONFIG_DIR.'/'.$configFileName;
+			if (is_file($commandConfigFilePath)) {
+				$configFileNames[] = $configFileName;
+			}
+		}
+		
+		foreach ($configFileNames as $configFileName) {
+			$dotEnv = new Dotenv(self::CONFIG_DIR, $configFileName);
+			$dotEnv->load();
+		}
+	}
+	
+	private static function exportCoreConfig() {
+		CoreConfig::$verboseMode = boolval(getenv('VERBOSE'));
 	}
 }
 
-$keybaseApiClient = new KeybaseAPIClient();
-
-$bot = new Bot($keybaseApiClient, EnabledCommands::getEnabledCommandClasses());
-$bot->run();
