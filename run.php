35bb0b8e87bff5a9d836b590a6c580ab46cbe631
Tim Strijdhorst <tim@decorrespondent.nl>
2017-09-30 18:06:31 +0200

    -giphy command from runner

--- run.php
+++ run.php
@@ -1,16 +1,12 @@
 <?php
 
-namespace Capetown;
-
 use Capetown\Core\Bot;
 use Capetown\Core\KeybaseAPIClient;
-use Capetown\Plugins\Giphy\GiphyCommand;
 
 require_once __DIR__.'/vendor/autoload.php';
 define('Capetown\VERBOSE', true);
 
 $enabledCommands = [
-	GiphyCommand::class
 ];
 
 $keybaseApiClient = new KeybaseAPIClient();
