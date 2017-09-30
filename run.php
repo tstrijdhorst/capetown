66eac31a25d36e83c6102a4df1921ebd9f472e76
Tim Strijdhorst <tim@decorrespondent.nl>
2017-09-30 18:41:48 +0200

    +new config structure

--- run.php
+++ run.php
@@ -1,15 +1,29 @@
 <?php
 
+use Capetown\Commands\Giphy\GiphyCommand;
 use Capetown\Core\Bot;
+use Capetown\Core\CommandInterface;
 use Capetown\Core\KeybaseAPIClient;
+use Dotenv\Dotenv;
 
 require_once __DIR__.'/vendor/autoload.php';
 define('Capetown\VERBOSE', true);
 
-$enabledCommands = [
+$enabledCommandClasses = [
+	GiphyCommand::class
 ];
+
+$configDir = __DIR__.'/config';
+
+foreach($enabledCommandClasses as $commandClass) {
+	$fileName = $commandClass::getName().'.env';
+	if (is_file($configDir.'/'.$fileName)) {
+		$dotEnv = new Dotenv($configDir, $commandClass::getName().'.env');
+		$dotEnv->load();
+	}
+}
 
 $keybaseApiClient = new KeybaseAPIClient();
 
-$bot = new Bot($keybaseApiClient, $enabledCommands);
+$bot = new Bot($keybaseApiClient, $enabledCommandClasses);
 $bot->run();
