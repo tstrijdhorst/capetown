eb74d0db6c6bfa58f0b06087cd03c148f395f869
Tim Strijdhorst <tim@decorrespondent.nl>
2017-10-19 22:22:10 +0200

    +adapter to install composer with an added set of requirements

---
+++ app/PluginManager/ComposerAdapter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Capetown\Runner\PluginManager;
+
+use Capetown\Runner\Constants;
+
+class ComposerAdapter {
+	public function installPlugins() {
+		$pluginRequirements = json_decode(file_get_contents(Constants::BASEDIR, 'plugins.json'), true);
+		
+		$composerPath         = Constants::BASEDIR.'composer.json';
+		$composerFileOriginal = file_get_contents($composerPath);
+		$composerArray        = json_decode($composerFileOriginal, true);
+		
+		$composerArrayWithPluginRequirements = array_merge($composerArray['require'], $pluginRequirements);
+		
+		file_put_contents(json_encode($composerArrayWithPluginRequirements), $composerPath);
+		
+		exec(getenv('COMPOSER_PATH').' install', $output);
+		
+		file_put_contents(json_encode($composerFileOriginal), $composerPath);
+	}
+}
