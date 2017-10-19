7eb79e4c91619a9874049738254a52a5328bd28e
Tim Strijdhorst <tim@decorrespondent.nl>
2017-10-19 22:43:14 +0200

    %previous refactor confused 2 concepts

--- app/PluginManager/PluginManager.php
+++ app/PluginManager/PluginManager.php
@@ -5,19 +5,17 @@ namespace Capetown\Runner\PluginManager;
 use Capetown\Runner\Constants;
 
 class PluginManager {
-	public function installPlugins() {
-		$pluginRequirements = json_decode(file_get_contents(Constants::BASEDIR, 'plugins.json'), true)['require'];
-		
+	public function installPlugins(array $pluginRequirements): void {
 		$composerPath         = Constants::BASEDIR.'composer.json';
 		$composerFileOriginal = file_get_contents($composerPath);
 		$composerArray        = json_decode($composerFileOriginal, true);
 		
-		$composerArrayWithPluginRequirements = array_merge($composerArray['require'], $pluginRequirements);
+		$composerArray['require'] = array_merge($composerArray['require'], $pluginRequirements);
 		
-		file_put_contents(json_encode($composerArrayWithPluginRequirements), $composerPath);
+		file_put_contents(json_encode($composerArray), $composerPath);
 		
 		exec(getenv('COMPOSER_PATH').' install', $output);
 		
-		file_put_contents(json_encode($composerFileOriginal), $composerPath);
+		file_put_contents($composerFileOriginal, $composerPath);
 	}
 }
