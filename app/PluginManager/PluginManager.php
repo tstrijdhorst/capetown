a492ab2177c65f643e79fd790484b1b6b22a85ff
Tim Strijdhorst <tim@decorrespondent.nl>
2017-10-24 16:33:41 +0200

    %refactor to constants

--- app/PluginManager/PluginManager.php
+++ app/PluginManager/PluginManager.php
@@ -5,22 +5,26 @@ namespace Capetown\Runner\PluginManager;
 use Capetown\Runner\Constants;
 
 class PluginManager {
+	const ENABLED_COMMANDS_PATH = Constants::CONFIGDIR.'enabledCommmands.json';
+	
+	const COMPOSER_PATH = Constants::BASEDIR.'composer.json';
+	
 	public function installPlugins(array $pluginRequirements): void {
-		$composerPath         = Constants::BASEDIR.'composer.json';
-		$composerFileOriginal = file_get_contents($composerPath);
+		$composerFileOriginal = file_get_contents(self::COMPOSER_PATH);
 		$composerArray        = json_decode($composerFileOriginal, true);
 		
 		$composerArray['require'] = array_merge($composerArray['require'], $pluginRequirements);
 		
 		try {
-			file_put_contents($composerPath, json_encode($composerArray));
+			file_put_contents(self::COMPOSER_PATH, json_encode($composerArray));
 			$this->composerUpdate($pluginRequirements);
+			$this->refreshEnabledCommandsConfig();
 		}
 		catch(\Throwable $e) {
 			throw $e;
 		}
 		finally {
-			file_put_contents($composerPath, $composerFileOriginal);
+			file_put_contents(self::COMPOSER_PATH, $composerFileOriginal);
 		}
 	}
 	
@@ -36,5 +40,18 @@ class PluginManager {
 		}
 		
 		exec($commandString, $output);
+	}
+	
+	private function refreshEnabledCommandsConfig(): void {
+		$enabledCommandsFQNSPre = json_decode(file_get_contents(self::ENABLED_COMMANDS_PATH), true);
+		
+		$enabledCommandsFQNSPost = [];
+		foreach ($enabledCommandsFQNSPre as $enabledCommandFQN) {
+			if (class_exists($enabledCommandFQN)) {
+				$enabledCommandsFQNSPost[] = $enabledCommandFQN;
+			}
+		}
+		
+		file_put_contents(self::ENABLED_COMMANDS_PATH, json_encode($enabledCommandsFQNSPost));
 	}
 }
