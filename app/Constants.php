d0668ab092e6b8692fa26386983b16c19cbecd86
Tim Strijdhorst <tim@decorrespondent.nl>
2017-10-26 23:45:17 +0200

    +all paths should always end in /

--- app/Constants.php
+++ app/Constants.php
@@ -4,6 +4,6 @@ namespace Capetown\Runner;
 
 class Constants {
 	const BASEDIR   = __DIR__.'/../';
-	const APPDIR    = self::BASEDIR.'app';
-	const CONFIGDIR = self::BASEDIR.'config';
+	const APPDIR    = self::BASEDIR.'app/';
+	const CONFIGDIR = self::BASEDIR.'config/';
 }
