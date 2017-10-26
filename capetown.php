e0bc6e6c47900421bd7bd6d6bcc92877c5a180d1
Tim Strijdhorst <tim@decorrespondent.nl>
2017-10-26 23:38:22 +0200

    +inject static code analyzer

--- capetown.php
+++ capetown.php
@@ -1,8 +1,8 @@
 <?php
 
 use Capetown\Runner\Bootstrapper;
-use Capetown\Runner\Constants;
 use Capetown\Runner\PluginManager\PluginManager;
+use Capetown\Runner\PluginManager\StaticCodeAnalyzer;
 
 require_once __DIR__.'/vendor/autoload.php';
 Bootstrapper::bootstrap();
@@ -13,6 +13,6 @@ if ($argc < 2 || ($argv[1] !== 'plugin' && $argv[2] !== 'install')) {
 }
 
 echo 'Installing plugins'.PHP_EOL;
-$pluginManager = new PluginManager();
+$pluginManager = new PluginManager(new StaticCodeAnalyzer());
 
-$pluginManager->installPlugins($pluginRequirements);
+$pluginManager->installPlugins();
