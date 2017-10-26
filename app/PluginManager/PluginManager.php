14f9370ff0e43159f024d2fc59dafd958ef9e501
Tim Strijdhorst <tim@decorrespondent.nl>
2017-10-26 22:53:46 +0200

    %refactor code analyzing into own service

--- app/PluginManager/PluginManager.php
+++ app/PluginManager/PluginManager.php
@@ -2,14 +2,19 @@
 
 namespace Capetown\Runner\PluginManager;
 
-use Capetown\Core\CommandInterface;
 use Capetown\Runner\Constants;
 
 class PluginManager {
 	const ENABLED_COMMANDS_PATH = Constants::CONFIGDIR.'enabledCommmands.json';
 	const COMPOSER_PATH         = Constants::BASEDIR.'composer.json';
+	const PLUGIN_PATH           = Constants::BASEDIR.'plugins.json';
 	
-	const PLUGIN_PATH = Constants::BASEDIR.'plugins.json';
+	/** @var StaticCodeAnalyzer */
+	private $staticCodeAnalyzer;
+	
+	public function __construct(StaticCodeAnalyzer $staticCodeAnalyzer) {
+		$this->staticCodeAnalyzer = $staticCodeAnalyzer;
+	}
 	
 	public function installPlugins(): void {
 		$pluginRequirements = $this->getPluginRequirements();
@@ -88,110 +93,12 @@ class PluginManager {
 			
 			foreach ($phpFilePaths as $phpFilePath) {
 				//@todo convert this to php 7.2 syntax
-				if ($this->implementsCommandInterface($phpFilePath)) {
-					$enabledCommandsFQNSPost = $this->getFQNFromStaticFile($phpFilePath);
+				if ($this->staticCodeAnalyzer->implementsCommandInterface($phpFilePath)) {
+					$enabledCommandsFQNSPost = $this->staticCodeAnalyzer->getFQN($phpFilePath);
 				}
 			}
 		}
 		
 		file_put_contents(self::ENABLED_COMMANDS_PATH, json_encode($enabledCommandsFQNSPost));
-	}
-	
-	private function getFQNFromStaticFile(string $phpFilePath): string {
-		$contents = file_get_contents($phpFilePath);
-		
-		$namespace  = '';
-		$className  = '';
-		
-		//Go through each token and evaluate it as necessary
-		$tokens = token_get_all($contents);
-		for ($i = 0; $i < count($tokens); $i++) {
-			if (is_array($tokens[$i]) && $tokens[$i][0] === T_NAMESPACE) {
-				//Current token is `namespace` the actual namespace starts at the next one
-				while ($tokens[++$i] !== ';') {
-					//Add all the namespace tokens until we hit a ;
-					if (is_array($tokens[$i]) && in_array($tokens[$i][0], [T_STRING, T_NS_SEPARATOR])) {
-						$namespace .= $tokens[$i][1];
-					}
-				}
-				
-				continue;
-			}
-			
-			if (is_array($tokens[$i]) && $tokens[$i][0] === T_CLASS) {
-				while ($tokens[++$i] !== '{') {
-					if (is_array($tokens[$i]) && $tokens[$i][0] === T_STRING) {
-						$className .= $tokens[$i][1];
-						break; //Classname is always one word
-					}
-				}
-			}
-		}
-		
-		return $namespace !== '' ? $namespace.'\\'.$className : $className;
-	}
-	
-	public function implementsCommandInterface($phpFilePath) {
-		$contents = file_get_contents($phpFilePath);
-		
-		$interfaces = [];
-		
-		$includesInterface = false;
-		$alias             = 'CommandInterface';
-		//Go through each token and evaluate it as necessary
-		$tokens     = token_get_all($contents);
-		$tokenCount = count($tokens);
-		for ($i = 0; $i < $tokenCount; $i++) {
-			//Check if the FQN of our command interface is used in the code
-			if ($includesInterface === false && is_array($tokens[$i]) && $tokens[$i][0] === T_USE) {
-				$usedFQN = '';
-				//Current token is `namespace` the actual namespace starts at the next one
-				while ($tokens[++$i] !== ';') {
-					//Add all the namespace tokens until we hit a ;
-					if (is_array($tokens[$i])) {
-						$token = $tokens[$i][0];
-						if (in_array($token, [T_STRING, T_NS_SEPARATOR])) {
-							$usedFQN .= $tokens[$i][1];
-						}
-						
-						if ($tokens[$i][0] === T_AS) {
-							$i     += 2; //next token is a whitespace, after that it's the alias
-							$alias = $tokens[$i][1];
-						}
-					}
-				}
-				$includesInterface = ($usedFQN === CommandInterface::class);
-				continue;
-			}
-			
-			//Get a list of all implemented interfaces
-			if (is_array($tokens[$i]) && $tokens[$i][0] === T_IMPLEMENTS) {
-				while ($tokens[++$i] !== '{') {
-					if (is_array($tokens[$i]) && $tokens[$i][0] === T_NS_SEPARATOR) {
-						$interfaceName = '';
-						while($tokens[++$i] !== ',') {
-							$interfaceName .= $tokens[$i][1];
-						}
-						$interfaces[] = $interfaceName;
-						continue;
-					}
-					
-					if (is_array($tokens[$i]) && in_array($tokens[$i][0], [T_STRING, T_NS_SEPARATOR])) {
-						$interfaces[] = $tokens[$i][1];
-					}
-				}
-				continue;
-			}
-		}
-		
-		if (in_array(CommandInterface::class, $interfaces, true)) {
-			return true;
-		}
-		
-		if ($includesInterface && in_array($alias, $interfaces, true)) {
-			return true;
-		}
-		
-		return false;
 	}
 }
