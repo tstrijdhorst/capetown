96cebd9d7aa393661230de0c853de58c133125ee
Tim Strijdhorst <tim@decorrespondent.nl>
2017-11-27 16:04:40 +0100

    %include proper Application

--- capetown.php
+++ capetown.php
@@ -4,7 +4,7 @@ use Capetown\Runner\Bootstrapper;
 use Capetown\Runner\PluginManager\ConsoleCommands\UpdateCommand;
 use Capetown\Runner\PluginManager\PluginManager;
 use Capetown\Runner\PluginManager\StaticCodeAnalyzer;
-use Composer\Console\Application;
+use Symfony\Component\Console\Application;
 
 require_once __DIR__.'/vendor/autoload.php';
 Bootstrapper::bootstrap();
