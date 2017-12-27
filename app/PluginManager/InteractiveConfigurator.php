370a479abd297e47f827e8ff3564494381c99eb5
Tim Strijdhorst <tim@decorrespondent.nl>
2017-12-27 17:47:55 +0100

    +ask for config values +transform map to env file +write to disk on demand

--- app/PluginManager/InteractiveConfigurator.php
+++ app/PluginManager/InteractiveConfigurator.php
@@ -2,8 +2,11 @@
 
 namespace Capetown\Runner\PluginManager\ConsoleCommands;
 
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 
 class InteractiveConfigurator {
 	/** @var InputInterface */
@@ -12,14 +15,45 @@ class InteractiveConfigurator {
 	private $output;
 	/** @var array */
 	private $configMap;
+	/** @var string */
+	private $configFilePath;
+	/** @var QuestionHelper */
+	private $questionHelper;
 	
-	public function __construct(string $configFilePath, InputInterface $input, OutputInterface $output) {
-		$this->input     = $input;
-		$this->output    = $output;
-		$this->configMap = $this->transformEnvFileToKeyValueMap($configFilePath);
+	public function __construct(string $configFilePath, InputInterface $input, OutputInterface $output, QuestionHelper $questionHelper) {
+		$this->input          = $input;
+		$this->output         = $output;
+		$this->configMap      = $this->transformEnvFileToKeyValueMap($configFilePath);
+		$this->configFilePath = $configFilePath;
+		$this->questionHelper = $questionHelper;
 	}
 	
-	private function transformEnvFileToKeyValueMap(string $configFilePath) {
+	public function askForConfigValues(): void {
+		$this->output->writeln('Configuring '.$this->configFilePath);
+		
+		$configMapProvided = [];
+		foreach ($this->configMap as $key => $valueDefault) {
+			$valueProvided = $this->questionHelper->ask(
+				$this->input,
+				$this->output,
+				new Question($key, (string)$valueDefault)
+			);
+			
+			$configMapProvided[$key] = $valueProvided;
+		}
+		
+		$writeToDisk = $this->questionHelper->ask(
+			$this->input,
+			$this->output,
+			new ConfirmationQuestion('Write the configuration to disk?', false)
+		);
+		
+		if ($writeToDisk) {
+			file_put_contents($this->configFilePath, $this->transformKeyValueMapToEnvFile($this->configMap));
+		}
+	}
+	
+	private function transformEnvFileToKeyValueMap(string $configFilePath): array {
 		$contents    = file_get_contents($configFilePath);
 		$configLines = explode("\n", $contents);
 		
@@ -34,5 +68,20 @@ class InteractiveConfigurator {
 		}
 		
 		return $configMap;
+	}
+	
+	private function transformKeyValueMapToEnvFile(array $configMap): string {
+		$envFile = '';
+		foreach ($configMap as $key => $value) {
+			$envFile = $key.'=';
+			
+			if ($value !== null) {
+				$envFile .= $value;
+			}
+			
+			$envFile .= "\n";
+		}
+		
+		return $envFile;
 	}
 }
